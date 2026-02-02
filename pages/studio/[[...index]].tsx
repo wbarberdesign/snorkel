dd9b953609ca94a5668549949b47f54b33f54ffc
William Barber <47955753+wbarberdesign@users.noreply.github.com>
2026-02-02 11:36:27 +0000

    Update [[...index]].tsx

--- pages/studio/[[...index]].tsx
+++ pages/studio/[[...index]].tsx
@@ -1,22 +1,6 @@
-import Head from 'next/head'
 import { NextStudio } from 'next-sanity/studio'
-import { NextStudioHead } from 'next-sanity/studio/head'
-import { StudioLayout, StudioProvider } from 'sanity'
-import config from 'sanity.config'
+import config from '../../sanity.config'
 
 export default function StudioPage() {
-  return (
-    <>
-      <Head>
-        <NextStudioHead favicons={false} />
-      </Head>
-
-      <NextStudio config={config}>
-        <StudioProvider config={config}>
-          {/* <GlobalStyle /> */}
-          <StudioLayout />
-        </StudioProvider>
-      </NextStudio>
-    </>
-  )
+  return <NextStudio config={config} />
 }
